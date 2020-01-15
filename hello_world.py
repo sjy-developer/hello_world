ee00df3b8fff662751e95bc1dd2c3324750c29f6
苏静怡 <jingyi.su@yiducloud.cn>
2020-01-15 17:11:15 +0800

    first commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+#import logging
+#LOG = logging.getLogger(__name__)
+
+@app.route('/')
+def ping():
+    return 'Hello World ya!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5555)
+
+
